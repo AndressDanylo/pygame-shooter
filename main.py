8bd5a789dd3286d8f0637b53ebce69b1ab7f73cf
Valkirpod <valdemarpodlipski2003@gmail.com>
2024-12-17 20:48:47 +0200

    Added screen & text file
    Empty screen that does not respond and a general purpose text file for things to do.

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 1366
+SCREEN_HEIGHT = 768
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+    pass
+
+pygame.quit()
