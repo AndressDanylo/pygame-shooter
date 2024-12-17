e852ddcc4a1599d7d5acd2b14e6c8c84761c227d
AndressDanylo <andreievdanyil.se@gmail.com>
2024-12-17 22:01:36 +0200

    Added player surface

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import pygame
 
+# pygame setup
 pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
 
-SCREEN_WIDTH = 1366
-SCREEN_HEIGHT = 768
+# player
+player_surface = pygame.image.load('assets/Player.png').convert_alpha()
+player_rectangle = player_surface.get_rect(topleft = (0, 0))
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while True:
-    pass
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("gray")
+    screen.blit(player_surface, player_rectangle)
+
+    # RENDER YOUR GAME HERE
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
