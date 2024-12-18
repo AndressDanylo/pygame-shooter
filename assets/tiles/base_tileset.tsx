19cc9e715301ad87bfdfdbc4a047d52382cb11d0
Valkirpod <valdemarpodlipski2003@gmail.com>
2024-12-18 14:20:54 +0200

    Added isCollidable attribute to Tile
    Tile has isCollidable set true if you had set a collidable property inside the TileSet.

--- assets/tiles/base_tileset.tsx
+++ assets/tiles/base_tileset.tsx
@@ -1,5 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="base_tileset" tilewidth="64" tileheight="64" tilecount="2" columns="2">
  <image source="base.png" width="128" height="64"/>
- <tile id="0" type="collidable"/>
+ <tile id="0" type="collidable">
+  <properties>
+   <property name="collidable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collidable" type="bool" value="false"/>
+  </properties>
+ </tile>
 </tileset>
--- map.py
+++ map.py
@@ -8,26 +8,27 @@ class Map:
         self.tiles = pygame.sprite.Group()
         self.height, self.width = 0, 0
 
-        # TODO load_pygame gives tile surfaces, if i want to get properties i'll need to use pytmx.TileMap i think.
         tmx_data = pytmx.util_pygame.load_pygame(self.map_file)
         self.width = tmx_data.width * tmx_data.tilewidth
         self.height = tmx_data.height * tmx_data.tileheight
-        print("SELF", self.width/64, self.height/64)
         for layer in tmx_data.visible_layers:
             for x, y, surface in layer.tiles():
+                gid = tmx_data.get_tile_gid(x, y, tmx_data.layers.index(layer))
+                props = tmx_data.get_tile_properties_by_gid(gid)
                 pos_x = x * tmx_data.tilewidth - self.width // 2
                 pos_y = y * tmx_data.tileheight - self.height // 2
-                Tile((pos_x, pos_y), surface, self.tiles)
+                Tile((pos_x, pos_y), surface, props, self.tiles)
     
     def draw(self, surface, pos_x, pos_y):
+        # TODO to make this performant get tiles that are only near the camera. maybe get screen rect that gets put to x,y and check what tiles collide???
         for sprite in self.tiles:
             rect = sprite.rect.move(pos_x, pos_y)
             surface.blit(sprite.image, rect)
-        # gotta move the map :(
         #self.tiles.draw(surface)
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, groups):
+    def __init__(self, pos, surface, props, groups):
         super().__init__(groups)
         self.image = surface
         self.rect = self.image.get_rect(topleft = pos)
+        self.isCcollidable = props.get("collidable")
