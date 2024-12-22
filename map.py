2d45182a54beebcaa0350022daecfe08f4c609b9
Valkirpod <valdemarpodlipski2003@gmail.com>
2024-12-23 01:10:43 +0200

    Refactored map.py

--- map.py
+++ map.py
@@ -8,18 +8,34 @@ class Map:
         self.map_file = map_file
         self.tiles = pygame.sprite.Group()
         self.collidable_tiles = pygame.sprite.Group()
-        self.height, self.width = 0, 0
-        self.pos_x, self.pos_y = 0, 0
 
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.pos_x = 0
+        self.pos_y = 0
+
+        self.load_map()
+
+    # TODO maybe turn map into a singleton which can reload into diff maps
+    def load_map(self):
         tmx_data = pytmx.util_pygame.load_pygame(self.map_file)
+        self.tile_size = tmx_data.tilewidth
         self.width = tmx_data.width * tmx_data.tilewidth
         self.height = tmx_data.height * tmx_data.tileheight
-        for object in tmx_data.get_layer_by_name("Meta Layer"):
+
+        self._set_spawn_position(tmx_data)
+        self._load_layers(tmx_data)
+    
+    def _set_spawn_position(self, tmx_data):
+         for object in tmx_data.get_layer_by_name("Meta Layer"):
             if object.name == "spawn":
-                        self.pos_x = object.x - screen_width // 2
-                        self.pos_y = object.y - screen_height // 2
+                        self.pos_x = object.x - self.screen_width // 2
+                        self.pos_y = object.y - self.screen_height // 2
+    
+    def _load_layers(self, tmx_data):
         for layer in tmx_data.layers:
             if isinstance(layer, pytmx.TiledTileLayer):
+                # Tiles
                 for x, y, surface in layer.tiles():
                     gid = tmx_data.get_tile_gid(x, y, tmx_data.layers.index(layer))
                     props = tmx_data.get_tile_properties_by_gid(gid)
@@ -29,15 +45,17 @@ class Map:
                     if props.get("collidable"):
                         self.collidable_tiles.add(tile)
             if isinstance(layer, pytmx.TiledObjectGroup):
+                # Objects
                 for object in layer:
                     pass
-    
-    def draw(self, surface, pos_x, pos_y):
-        # TODO to make this performant get tiles that are only near the camera. maybe get screen rect that gets put to x,y and check what tiles collide???
-        for sprite in self.tiles:
-            rect = sprite.rect.move(pos_x, pos_y)
-            surface.blit(sprite.image, rect)
-        #self.tiles.draw(surface)
+
+    def draw(self, display, pos_x, pos_y):
+        display_rect = display.get_rect()
+        #display_rect.inflate_ip(-64*4, -64*2)
+        for tile in self.tiles:
+            tile_rect = tile.rect.move(pos_x, pos_y)
+            if display_rect.colliderect(tile_rect):
+                display.blit(tile.image, tile_rect)
 
     def get_collidable_tiles(self):
         return self.collidable_tiles
@@ -47,4 +65,4 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surface
         self.rect = self.image.get_rect(topleft = pos)
-        self.isCcollidable = props.get("collidable")
+        self.is_collidable = props.get("collidable", False)
